744f41de876c92834c255c9846b64aa21590cc10
HarshanNaik18 <harshannaik18@gmail.com>
2023-07-02 14:29:13 +0530

    Firebase Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
+import React from 'react'
 import './App.css';
+
+import { auth } from './Firebase/Firebase';
 
 function App() {
   return (
     <div className="App">
-      YE
+      <lable>DTN</lable>
+      <lable>HDN</lable>
+
     </div>
   );
 }
